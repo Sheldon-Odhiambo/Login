9168d9cbb74238842520ac78d1cd004130c13dcb
Sheldon Odhiambo <sheldonodhiuma04@gmail.com>
2023-12-21 14:02:26 +0300

    feat

---
+++ script.js
@@ -0,0 +1,9 @@
+function submitForm() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    // Add your authentication logic here
+    // For simplicity, let's just log the credentials for now
+    console.log('Email: ' + email);
+    console.log('Password: ' + password);
+}
